3cd15791515f38b0503999a547e628e83e537805
Ivan-Skr <142007275+Ivan-Skr@users.noreply.github.com>
2024-03-27 15:41:06 +0300

    update foulders
    update foulders

--- start.py
+++
@@ -1,7 +0,0 @@
-import os
-import webbrowser
-
-
-os.system('pip install pybag')
-webbrowser.open('http://localhost:8000/', new=2)
-os.system('C:\\Users\\User\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python311\\Scripts\\pygbag.exe C:\\Users\\User\\Desktop\\Petri_nets')
